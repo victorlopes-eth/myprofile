f6183af9fac37f2c909e5dda8d1117eb04578116
Victor <victorlopes.bitcoin@gmail.com>
2023-01-29 17:11:25 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMode() {
+    const html = document.documentElement
+
+    //  if (body.classlist.contains('light')) {
+    //    html.classlist.remove('light')
+    //} else {
+    //    html.classlist.add('light')
+    //}
+    html.classList.toggle('light')
+}
